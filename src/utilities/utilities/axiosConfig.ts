17e94036c6c27d473b17730269d8ae6a20ab12af
Carolina Blandon <carolinamasielblandon@gmail.com>
2026-03-16 19:48:34 -0600

    correción estructura de carpetas

---
+++ src/utilities/utilities/axiosConfig.ts
@@ -0,0 +1,11 @@
+import { AxiosRequestConfig } from 'axios';
+
+const config: AxiosRequestConfig = {
+    baseURL: import.meta.env.VITE_URL_API,
+    headers: {
+        'content-type': 'application/json',
+        'X-Requested-With': 'XMLHttpRequest'
+    }
+}
+
+export { config };
