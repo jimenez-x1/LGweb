70d22771de2aad9f036032f9d7d97d33adb3c391
Jesé Obed Manzanares Cerrato <jesecerrato17@gmail.com>
2026-03-24 23:53:26 -0600

    Archivos ts

---
+++ src/utilities/TypeUtilities.ts
@@ -0,0 +1,24 @@
+import { Type as TypeError } from '../Api/namespaces/errorService';
+
+export type TypeUtilities = {
+    url: string;
+    data?: Array<object> | object;
+    key?: string;
+}
+
+export type TypeGenericResponse = {
+    singleData: object;
+    data: Array<object>
+    status: number;
+    error: TypeError.ErrorSchema;
+}
+
+export const INIT: TypeGenericResponse = {
+    singleData: {},
+    data: [],
+    error: {
+        code: 0,
+        message: ""
+    },
+    status: 0
+};
