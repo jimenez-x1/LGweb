9417231afb60410a0051329a119e6155a64b134b
David Sanabria <sanabriadavid112@gmail.com>
2026-04-10 22:49:50 -0600

    feat: módulo maestros completo - CRUD y frontend

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Alumnos from "./pages/Alumnos";
 import RegistrarAlumno from "./pages/RegistrarAlumno";
+import Maestros from "./pages/Maestros";
+import RegistrarMaestro from "./pages/RegistrarMaestro";
+import EditarMaestro from "./pages/EditarMaestro";
 
 function App() {
   return (
@@ -9,6 +12,9 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/alumnos" element={<Alumnos />} />
       <Route path="/registrar-alumno" element={<RegistrarAlumno />} />
+      <Route path="/maestros" element={<Maestros />} />
+      <Route path="/registrar-maestro" element={<RegistrarMaestro />} />
+      <Route path="/editar-maestro/:id" element={<EditarMaestro />} />
     </Routes>
   );
 }
