ed989fa4286b50d22d0b098735ff15312a2486f8
Sanacdd <sanabriadavid112@gmail.com>
2026-07-07 20:01:37 -0600

    Fix: evitar condición de carrera en Utilities.ts al compartir objeto de respuesta entre peticiones paralelas

--- src/utilities/Utilities.ts
+++ src/utilities/Utilities.ts
@@ -5,7 +5,14 @@ import { config } from "./axiosConfig";
 import { isEmpty } from "lodash";
 const api = axios.create(config);
 
-const responseData: TypeGenericResponse = INIT;
+// Crea un objeto de respuesta NUEVO e independiente en cada llamada,
+// para evitar que peticiones en paralelo se pisen entre sí.
+function crearRespuestaVacia(): TypeGenericResponse {
+    return {
+        ...INIT,
+        error: { ...INIT.error },
+    };
+}
 
 api.interceptors.request.use(
     async config => {
@@ -21,6 +28,7 @@ api.interceptors.request.use(
 
 async function getData(props: TypeUtilities) {
     return await api.get(props.url).then(response => {
+        const responseData = crearRespuestaVacia();
         if (response['status'] === 401) {
             responseData.status = 401;
             return responseData
@@ -29,6 +37,7 @@ async function getData(props: TypeUtilities) {
         responseData.status = response.status;
         return responseData;
     }).catch(error => {
+        const responseData = crearRespuestaVacia();
         const response = error["response"];
         if (response["status"] === 401 || response["status"] === 404) {
             responseData.error.code = parseInt(response["status"], 10);
@@ -43,6 +52,7 @@ async function getData(props: TypeUtilities) {
 
 async function getSingleData(props: TypeUtilities) {
     return await api.get(props.url).then(response => {
+        const responseData = crearRespuestaVacia();
         const dataArray = response.data;
         if (response['status'] === 401) {
             responseData.status = 401;
@@ -52,6 +62,7 @@ async function getSingleData(props: TypeUtilities) {
         responseData.status = response.status;
         return responseData;
     }).catch(error => {
+        const responseData = crearRespuestaVacia();
         const response = error["response"];
         if (response["status"] === 401 || response["status"] === 404) {
             responseData.error.code = parseInt(response["status"], 10);
@@ -68,6 +79,7 @@ async function saveData(props: TypeUtilities) {
     const { data } = props;
     return await api.post(props.url, data)
         .then(response => {
+            const responseData = crearRespuestaVacia();
             if (response['status'] === 401) {
                 responseData.status = 401;
                 return responseData
@@ -77,6 +89,7 @@ async function saveData(props: TypeUtilities) {
             return responseData;
         })
         .catch(error => {
+            const responseData = crearRespuestaVacia();
             const response = error["response"];
             if (response["status"] === 401 || response["status"] === 404) {
                 responseData.error.code = parseInt(response["status"], 10);
@@ -96,6 +109,7 @@ async function updateData(props: TypeUtilities) {
     const { data } = props;
     return await api.put(props.url, data)
         .then(response => {
+            const responseData = crearRespuestaVacia();
             if (response['status'] === 401) {
                 responseData.status = 401;
                 return responseData
@@ -105,6 +119,7 @@ async function updateData(props: TypeUtilities) {
             return responseData;
         })
         .catch(error => {
+            const responseData = crearRespuestaVacia();
             const response = error["response"];
             if (response["status"] === 401 || response["status"] === 404) {
                 responseData.error.code = parseInt(response["status"], 10);
@@ -120,6 +135,7 @@ async function updateData(props: TypeUtilities) {
 async function deleteData(props: TypeUtilities) {
     return await api.delete(props.url)
         .then(response => {
+            const responseData = crearRespuestaVacia();
             if (response['status'] === 200) {
                 responseData.status = 200;
                 return responseData
@@ -129,6 +145,7 @@ async function deleteData(props: TypeUtilities) {
             return responseData;
         })
         .catch(error => {
+            const responseData = crearRespuestaVacia();
             const response = error["response"];
             if (response["status"] === 401 || response["status"] === 404) {
                 responseData.error.code = parseInt(response["status"], 10);
@@ -145,6 +162,7 @@ async function signUp(props: TypeUtilities) {
     const { data } = props;
     return await api.post(props.url, data)
         .then(response => {
+            const responseData = crearRespuestaVacia();
             if (response['status'] === 401) {
                 responseData.status = 401;
                 return responseData
@@ -154,6 +172,7 @@ async function signUp(props: TypeUtilities) {
             return responseData;
         })
         .catch(error => {
+            const responseData = crearRespuestaVacia();
             const response = error["response"];
             if (response["status"] === 401 || response["status"] === 404) {
                 responseData.error.code = parseInt(response["status"], 10);
@@ -174,6 +193,7 @@ async function LogIn(props: TypeUtilities) {
     const { data, url } = props;
     return await api.post(url, data)
         .then(response => {
+            const responseData = crearRespuestaVacia();
             if (response['status'] === 401) {
                 responseData.status = 401;
                 return responseData
@@ -184,6 +204,7 @@ async function LogIn(props: TypeUtilities) {
             return responseData;
         })
         .catch(error => {
+            const responseData = crearRespuestaVacia();
             const response = error["response"];
             if (response["status"] === 401 || response["status"] === 404) {
                 responseData.error.code = parseInt(response["status"], 10);
@@ -206,6 +227,7 @@ async function checkUser() {
     }
     return await api.get('/user/whoami')
         .then(response => {
+            const responseData = crearRespuestaVacia();
             if (response.status === 200) {
                 responseData.data = response.data;
                 responseData.status = response.status;
@@ -216,6 +238,7 @@ async function checkUser() {
                 return responseData
             }
         }).catch(error => {
+            const responseData = crearRespuestaVacia();
             const response = error["response"]
             if (response["status"] === 401 || response["status"] === 404) {
                 responseData.error.code = response["status"];
