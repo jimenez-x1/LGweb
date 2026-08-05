68a37e6dac4c31b2e44c3969ae12548d3786f292
jimenez-x1 <alejarquitectura@gmail.com>
2026-08-05 08:35:19 -0600

    feat: configure api url for production

--- src/utilities/axiosConfig.ts
+++ src/utilities/axiosConfig.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import type { AxiosRequestConfig } from "axios";
 
 export const config: AxiosRequestConfig = {
-  baseURL: "http://localhost:3000/api",
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     "Content-Type": "application/json",
     "X-Requested-With": "XMLHttpRequest",
